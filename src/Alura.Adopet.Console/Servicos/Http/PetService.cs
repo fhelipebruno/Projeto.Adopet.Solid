7648ca97e05c13b42f557b397a8ff26e9c67aa6e
Fhelipe Bruno <fhelipebruno@gmail.com>
2024-10-19 17:42:50 -0300

    Implementada classe ClienteService

--- src/Alura.Adopet.Console/Servicos/Http/PetService.cs
+++ src/Alura.Adopet.Console/Servicos/Http/PetService.cs
@@ -4,11 +4,11 @@ using Alura.Adopet.Console.Servicos.Abstracoes;
 
 namespace Alura.Adopet.Console.Servicos.Http;
 
-public class HttpClientPet: IApiService<Pet>
+public class PetService: IApiService<Pet>
 {
     private HttpClient client;
 
-    public HttpClientPet(HttpClient client)
+    public PetService(HttpClient client)
     {
         this.client = client;
     }
