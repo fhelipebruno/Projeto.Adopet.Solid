96e656e77016350f8db60e559a97a34921def479
Fhelipe Bruno <fhelipebruno@gmail.com>
2025-03-17 14:06:55 -0300

    IMplementação de importação de clientes

---
+++ src/Alura.Adopet.Console/Servicos/Arquivos/PetsDoJson.cs
@@ -0,0 +1,11 @@
+﻿using Alura.Adopet.Console.Modelos;
+
+namespace Alura.Adopet.Console.Servicos.Arquivos
+{
+    public class PetsDoJson : LeitorDeArquivosJson<Pet>
+    {
+        public PetsDoJson(string caminhoArquivo) : base(caminhoArquivo)
+        {
+        }
+    }
+}
---
+++ src/Alura.Adopet.Console/Servicos/ClienteService.cs
@@ -0,0 +1,9 @@
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos.Abstracoes;
+
+namespace Alura.Adopet.Console.Servicos
+{
+    internal class ClienteService : IApiService<Cliente>
+    {
+    }
+}
